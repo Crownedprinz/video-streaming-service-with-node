3b0b2b55d049d8b29b8aa63abb250aa32d1b9716
Ademola John <AdemolaJhon@gmail.com>
2021-03-30 16:52:27 +0100

    ammended chunk to a varying size

--- video.js
+++ video.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fs = require('fs');
-
+// get video stats (about 61MB)
+  let fileName = "bigbuck.mp4";
+  const videoPath = fileName;
+  const videoSize = fs.statSync(fileName).size;
 router.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -10,24 +13,33 @@ router.get("/1", (req, res, next) => {
   // Ensure there is a range given for the video
   const range = req.headers.range;
   if (!range) {
-    res.status(400).send("Requires Range header");
-  }
+      const head = {
+        "Content-Length": videoSize,
+        "Content-Type": "video/mp4",
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(videoPath).pipe(res);
+  }else{
 
-  // get video stats (about 61MB)
-  let fileName = "hunterfishing.mp4";
-  const videoPath = fileName;
-  const videoSize = fs.statSync(fileName).size;
+  
   // Parse Range
   // Example: "bytes=32324-"
-  const CHUNK_SIZE = 1024 ** 2; // 1MB
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//   const CHUNK_SIZE = 1024 ** 2; // 1MB
+//   const start = Number(range.replace(/\D/g, ""));
+//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  
+
+  let parts = range.replace(/bytes=/, "").split("-");
+  let start = parseInt(parts[0], 10);
+  let end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1;
+  let chunkSize = end - start + 1;
+
   // Create headers
   const contentLength = end - start + 1;
   const headers = {
     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
     "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
+    "Content-Length": chunkSize,
     "Content-Type": "video/mp4",
   };
 
@@ -40,6 +52,7 @@ router.get("/1", (req, res, next) => {
   // Stream the video chunk to the client
   videoStream.pipe(res);
   //res.sendFile(__dirname + "/index.html");
+}
 });
 
 module.exports = router;
